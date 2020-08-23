4d7eb9db8032e2083659e74cea1c8e3a69069d15
SharonG16 <sfbaylon@yahoo.com>
2020-08-23 11:49:50 -0700

    Added additional views/pages.

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Calgary Children Society' });
 });
 
-
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Calgary Children Society' });
+});
+/* GET volunteer page. */
+router.get('/volunteer', function(req, res, next) {
+  res.render('volunteer', { title: 'Calgary Children Society' });
+});
 
 module.exports = router;
 
