dbc03a1a14f4c84d9a7a9d09487bce13720dc039
SharonG16 <sfbaylon@yahoo.com>
2020-09-10 14:57:01 -0700

    Modified schedule route.

--- routes/schedules.js
+++ routes/schedules.js
@@ -2,9 +2,12 @@ var express = require('express');
 var scheduleController = require('../controllers/schedules');
 var router = express.Router();
 
+
+
 router 
   .route('/scheduleform')
   .get(scheduleController.signupForm)
   .post(scheduleController.create)
+
 
 module.exports = router;
